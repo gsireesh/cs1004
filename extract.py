ce148dd25c2a09a10c5664630ddf8dac614f3969
Sireesh Gururaja <skg2123@columbia.edu>
2014-09-22 21:52:11 -0400

    script now includes range functionality

--- extract.py
+++ extract.py
@@ -1,22 +1,27 @@
 '''
 USAGE : extract.py [ folder that contains assignments ] [ grade report file ]
+[ start UNI ] [ end UNI ]
 '''
 
 import sys 
 import os
-import subprocess
+import shutil
 import re
 
 
 
 pwd = os.getcwd()
 
-if(len(sys.argv) != 3):
-	relPath = input('Enter problem set/programming project directory:')
-	gradeReportPath = input('Enter name of grade report file:')
+if(len(sys.argv) != 5):
+	relPath = input('Enter problem set/programming project directory: ')
+	gradeReportPath = input('Enter name of grade report file: ')
+	startUNI = input('Enter first UNI in your group: ')
+	endUNI = input('Enter the last UNI in your group: ')
 else:
 	relPath = sys.argv[1]
 	gradeReportPath = sys.argv[2]
+	startUNI = sys.argv[3]
+	endUNI = sys.argv[4]
 
 gradeReport = open(gradeReportPath, 'r').read()
 
@@ -36,9 +41,14 @@ folderList = [f for f in os.listdir(folder) if folderRE.match(f)]
 
 for f in folderList:
 	uni = folderRE.match(f).group('uni')
-	os.rename(f, uni)
-	with open(uni+'.txt', 'w') as customGR: 
-		customGR.write(gradeReport)
+	if uni >= startUNI and uni <= endUNI: 
+		os.rename(f, uni)
+		with open(uni+'.txt', 'w') as customGR: 
+			customGR.write(gradeReport)
+	else: 
+		shutil.rmtree(f)
+
+
 
 
 
