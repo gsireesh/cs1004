2bd7879d75f6f99cf2549bd170e3d01893b134bc
Sireesh Gururaja <skg2123@columbia.edu>
2014-09-23 17:05:01 -0400

    slight improvements

--- tabulateScript.py
+++ tabulateScript.py
@@ -2,8 +2,10 @@ import os
 import re
 import math
 
+LINEWITHTOTAL = 3
+
 filesList = [f for f in os.listdir(os.getcwd()) if not os.path.isdir(f) and \
-(f.split('.')[1] == 'txt') and re.match('\D+\d{4}',f,0)]
+(f.split('.')[1] == 'txt') and re.match('\D{2,3}\d{4}',f,0)]
 filesList.sort()
 scoreTable = open('tabulatedScores.txt','w')
 stringofScores = []
@@ -12,7 +14,7 @@ stats = []
 for f in filesList:
     temp = open(f,'r')
     print(f)
-    score = re.search('(\d+.?\d)/\d+.?\d',temp.readlines()[3],0).group(1)
+    score = re.search('(\d+.?\d)/\d+.?\d',temp.readlines()[LINEWITHTOTAL],0).group(1)
     stringofScores.append(f.split('.')[0]+'\t\t'+score)
     stats.append(score)
     temp.close()
