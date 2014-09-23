43ef3fbbad7a6b90d3fc919ddb95d8a637a84964
Sireesh Gururaja <skg2123@columbia.edu>
2014-09-22 20:30:23 -0400

    added existing scripts

---
+++ extract.py
@@ -0,0 +1,45 @@
+'''
+USAGE : extract.py [ folder that contains assignments ] [ grade report file ]
+'''
+
+import sys 
+import os
+import subprocess
+import re
+
+
+
+pwd = os.getcwd()
+
+if(len(sys.argv) != 3):
+	relPath = input('Enter problem set/programming project directory:')
+	gradeReportPath = input('Enter name of grade report file:')
+else:
+	relPath = sys.argv[1]
+	gradeReportPath = sys.argv[2]
+
+gradeReport = open(gradeReportPath, 'r').read()
+
+folderRE = re.compile('.*, .*\((?P<uni>.*)\)')
+
+folder = pwd+'/'+relPath
+
+if not os.path.exists(folder):
+	print('Specified folder does not exist!')
+	sys.exit(1)
+
+
+os.chdir(folder)
+
+folderList = [f for f in os.listdir() if folderRE.match(f)]
+
+
+for f in folderList:
+	uni = folderRE.match(f).group('uni')
+	os.rename(f, uni)
+	with open(uni+'.txt', 'w') as customGR: 
+		customGR.write(gradeReport)
+
+
+
+
---
+++ send_grades_mutt.py
@@ -0,0 +1,19 @@
+import os, sys
+
+if __name__ == '__main__':
+
+
+	current_path = os.getcwd()
+
+	grades_folder = "hw1/" ##### wherever the grade reports are
+
+	grade_reports = os.listdir(grades_folder)
+	print sys.argv[1] #####pass in assignment #
+	for report in grade_reports:
+
+		if ".txt" in report:
+			uni = report.replace(".txt", "")
+			report = grades_folder + report
+			print uni+"\n"
+			os.system("mutt -s \"[cs1004] Programming Project "+str(sys.argv[1])+"\" -c your-uni@columbia.edu -c cannon@cs.columbia.edu "+uni+"@columbia.edu < "+report)
+			print report+" sent! \n"
