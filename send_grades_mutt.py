93eb7d5fd7e8e16141b454f9a99d9f7f45642dc0
Anusha Balakrishnan <anusha.bala28@gmail.com>
2014-09-25 15:11:34 -0400

    Modified mutt script so that it can be run from any directory, not just the directory with the grade reports.

--- send_grades_mutt.py
+++ send_grades_mutt.py
@@ -6,18 +6,20 @@ import os, sys
 if __name__ == '__main__':
 
     assignment_name = raw_input("What assignment are you grading? ")
+    grades_folder = raw_input("What folder are the grade reports stored in?")
+    if grades_folder[-1]!='/':
+        grades_folder += '/'
 
-	current_path = os.getcwd()
+    current_path = os.getcwd()
 
-	grades_folder = "hw"+ sys.argv[1] + "/" ##### wherever the grade reports are
+	#grades_folder = "hw"+ sys.argv[1] + "/" ##### wherever the grade reports are
 
-	grade_reports = os.listdir(grades_folder)
-	print sys.argv[1] #####pass in assignment #
-	for report in grade_reports:
-
-		if ".txt" in report:
-			uni = report.replace(".txt", "")
-			report = grades_folder + report
-			print uni+"\n"
-			os.system("mutt -s \"[cs1004] " + assignment_name +"\" -c your-uni@columbia.edu -c cannon@cs.columbia.edu "+uni+"@columbia.edu < "+report)
-			print report+" sent! \n"
+    grade_reports = os.listdir(grades_folder)
+	#print sys.argv[1] #####pass in assignment #
+    for report in grade_reports:
+	if ".txt" in report:
+		uni = report.replace(".txt", "")
+		report = grades_folder + report
+		print uni+"\n"
+		os.system("mutt -s \"[cs1004] " + assignment_name +"\" -c your-uni@columbia.edu -c cannon@cs.columbia.edu "+uni+"@columbia.edu < "+report)
+		print report+" sent! \n"
